60b5337eee95637b94c7a34ad575ff5b6ce1c05e
Shashank <shashank.keshava@bookmyshow.com>
2017-12-28 18:06:13 +0530

    Sample webpack build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,29 @@
-module.export={
-    entry:{
-        app:'./src/client/index.js'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path'); //Provides utilities for working with file and directory paths
 
-    }
-}
+/* Webpack Structure
+    Entry
+    Output
+    Loaders: Enable webpack to process more than just JavaScript files 
+    Plugins: Plugings Range from bundle optimization and minification all the way to defining environment-like variables.
+ */
+
+const fileMap = {};
+module.exports = {
+    entry: {
+        app: './src/client/index.js',
+    },
+    output: {
+        // path.resolve() method resolvd a sequence of paths or path segment into absolute path```
+        path: path.resolve(__dirname, 'public'),
+        filename: 'webpack.bundle.js',
+    },
+    module: {
+        rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+    ],
+};
