154f58523fce75e8d71bccdd48fcb97d3411fdd9
Shashank <shashank.keshava@bookmyshow.com>
2018-01-18 19:02:59 +0530

    Webpack Configuration with babel-loader implemented

--- webpack.config.js
+++ webpack.config.js
@@ -8,24 +8,27 @@ const path = require('path');
     Plugins: Plugings Range from bundle optimization and minification all the way to defining environment-like variables.
 */
 module.exports = {
-    // First file in your dependency graph(first file in app)
+    // Entry: First file webpack starts(your dependency graph)
     entry: {
         app: path.resolve(__dirname, './src/index.js'),
     },
+    // Output: How and where to put bundles and format them
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
     },
+    // Loaders:  How to treat files before adding to dependency graphs 
     module: {
-        rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-    },
-    //loaders
-    modules: {
         rules: [
             {
                 test: /\.js$/,
                 use: 'babel-loader',
-            }
+            },
         ],
     },
+    // Plugins: Extremely Customisable
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.ContextReplacementPlugin(),
+    ],
 };
