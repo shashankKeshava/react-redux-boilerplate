f362bd2a80122b010e1dc0309de476fd95d3093b
Shashank <shashank.keshava@bookmyshow.com>
2018-01-19 19:10:00 +0530

    Leveraging npm and functions in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,28 +7,30 @@ const path = require('path');
     Loaders: Modules Enable webpack to process more than just JavaScript files
     Plugins: Plugings Range from bundle optimization and minification all the way to defining environment-like variables.
 */
-module.exports = {
-    // Entry: First file webpack starts(your dependency graph)
-    entry: {
-        app: path.resolve(__dirname, './src/index.js'),
-    },
-    // Output: How and where to put bundles and format them
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    // Loaders:  How to treat files before adding to dependency graphs 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-            },
+module.exports = env => {
+    return {
+        // Entry: First file webpack starts(your dependency graph)
+        entry: {
+            app: path.resolve(__dirname, './src/index.js'),
+        },
+        // Output: How and where to put bundles and format them
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'public'),
+        },
+        // Loaders:  How to treat files before adding to dependency graphs
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader',
+                },
+            ],
+        },
+        // Plugins: Extremely Customisable
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin(),
+            new webpack.ContextReplacementPlugin(),
         ],
-    },
-    // Plugins: Extremely Customisable
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.ContextReplacementPlugin(),
-    ],
+    };
 };
